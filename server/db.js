25c137c0b9341bdc6392c68a1b7a723205a3c2f2
pinoshe <16742923+pinoshe@users.noreply.github.com>
2019-11-27 09:43:45 +0200

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,9 +1,7 @@
 "user strict";
 
 const MongoClient = require("mongodb").MongoClient;
-const uri =
-  "mongodb+srv://pinoshe:pinoshe@cluster0-3juuq.mongodb.net/test?retryWrites=true";
-//"mongodb+srv://pinoshe:pinoshe@cluster0-uxsrv.mongodb.net/test?retryWrites=true";
+const uri = '';
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 module.exports = client;
